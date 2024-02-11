413a63f7a87425b4683906999cfc6da00c4f1345
Shiva Khatri <shivakhatri@Shivas-MacBook-Air.local>
2024-02-10 23:17:16 -0500

    Adding some figures

--- main.py
+++ main.py
@@ -94,74 +94,144 @@ df_stocks = px.data.stocks()
 #                  hover_data=['petal_width']
 #                  )
 # fig.show()
+#
+# df_iris = px.data.iris()
+# fig=go.Figure()
+# fig.add_trace(go.Scatter(
+#     x=df_iris.sepal_width,
+#     y=df_iris.sepal_length,
+#     mode='markers',
+#     marker_color=df_iris.sepal_width,
+#     text=df_iris.species,
+#     marker=dict(
+#         showscale=True
+#     )
+# ))
+# fig.update_traces(marker_line_width=2, marker_size=10)
+#
+#
+#
+# fig = go.Figure(data=go.Scattergl(
+#     x=np.random.randn(100000),
+#     y=np.random.randn(100000),
+#     mode='markers',
+#     marker=dict(
+#         color=np.random.randn(100000),
+#         colorscale='Viridis',
+#         line_width=1
+#     )
+#
+# ))
+#
+#
+#
+# # Pie chart
+#
+# df_asia=px.data.gapminder().query("year==2007").query("continent=='Asia'")
+# fig = px.pie(df_asia, values='pop', names='country',
+#        title='Population of Asian Continent',
+#        color_discrete_sequence=px.colors.sequential.RdBu
+#        )
+#
+# colors=['blue', 'green', 'black', 'purple', 'red', 'brown']
+# fig = go.Figure(data=[go.Pie(labels=['Water', 'Grass', 'Normal', 'Psychic', 'Fire', 'Ground'], values=[110,90,80,80,70,60])])
+# fig.update_traces(hoverinfo='label+percent', textfont_size=20, textinfo='label+percent',
+#                   pull=[0.1, 0, 0.2, 0, 0, 0],
+#                   marker=dict(colors=colors, line=dict(color='#FFFFFF', width=2))
+#                   )
+#
+# # Histogram
+# dice_1 = np.random.randint(1,7,5000)
+# dice_2 = np.random.randint(1,7,5000)
+#
+# dice_sum = dice_1 + dice_2
+# print(dice_sum)
+# fig  = px.histogram(dice_sum, nbins=11,
+#                     labels='5000 Dice Roll Histogram',
+#                     marginal='violin',
+#                     color_discrete_sequence=['green']
+#                     )
+# fig.update_layout(
+#     xaxis_title_text='Dice Roll',
+#     yaxis_title_text='Dice Sum',
+#     bargap=0.2, showlegend=False)
+#
+#
+# df_tips = px.data.tips()
+# fig = px.histogram(df_tips, x='total_bill', color='sex')
 
-df_iris = px.data.iris()
-fig=go.Figure()
-fig.add_trace(go.Scatter(
-    x=df_iris.sepal_width,
-    y=df_iris.sepal_length,
-    mode='markers',
-    marker_color=df_iris.sepal_width,
-    text=df_iris.species,
-    marker=dict(
-        showscale=True
-    )
-))
-fig.update_traces(marker_line_width=2, marker_size=10)
+#
+# fig = px.box(df_tips, x='sex', y='tip', points='all')
+#
+# fig = px.box(df_tips, x='day', y='tip',color='sex')
+# fig.show()
 
 
+# df_tips = px.data.tips()
+# fig = go.Figure()
+# fig.add_trace(go.Box(x=df_tips.sex, y=df_tips.tip, marker_color='blue',
+#                      boxmean='sd'))
+# fig.show()
 
-fig = go.Figure(data=go.Scattergl(
-    x=np.random.randn(100000),
-    y=np.random.randn(100000),
-    mode='markers',
-    marker=dict(
-        color=np.random.randn(100000),
-        colorscale='Viridis',
-        line_width=1
-    )
+# Violin Plots
 
-))
+#df_tips = px.data.tips()
+# fig = px.violin(df_tips, y="total_bill", box=True, points='all')
+# fig = px.violin(df_tips, y="tip", x="smoker", color="sex", box=True,
+#                 points="all", hover_data=df_tips.columns
+#                 )
+
+# fig = go.Figure()
+#
+# fig.add_trace(go.Violin(x=df_tips['day'][df_tips['smoker'] == 'Yes'],
+#                         y=df_tips['total_bill'][df_tips['smoker'] == 'Yes'],
+#                         legendgroup='Yes', scalegroup='Yes', name='Yes',
+#                         side='negative', line_color='blue'
+#
+#                         ))
+# fig.add_trace(go.Violin(x=df_tips['day'][df_tips['smoker'] == 'No'],
+#                         y=df_tips['total_bill'][df_tips['smoker'] == 'No'],
+#                         legendgroup='Yes', scalegroup='Yes', name='No',
+#                         side='positive', line_color='red'
+#
+#                         ))
+
+# fig.show()
+
+# # Density Heatmaps
+# flights = sns.load_dataset("flights")
+#
+# fig = px.density_heatmap(flights, x='year',y='month', z='passengers', color_continuous_scale='Viridis')
+# fig.show()
+
+# 3D Scatter Plots
+#
+# fig = px.scatter_3d(flights, x='year', y='month', z='passengers', color='year', opacity=0.7)
+#
+# fig.show()
+
+# Line Plots
+
+# fig = px.line_3d(flights, x='year', y='month', z='passengers', color='year')
+#
+# fig.show()
+
+# Scatter Matrix
+# flights = sns.load_dataset("flights")
+# fig = px.scatter_matrix(flights, color ='month')
+# fig.show()
+
+# Map Scatter Plots
+
+# df = px.data.gapminder().query("year == 2007")
+# fig = px.scatter_geo(df, locations="iso_alpha",
+#                      color="continent",
+#                      hover_name="country",
+#                      size="pop",
+#                      projection="orthographic"
+#                      )
+# fig.show()
+
+# Choropleth Maps
 
 
-
-# Pie chart
-
-df_asia=px.data.gapminder().query("year==2007").query("continent=='Asia'")
-fig = px.pie(df_asia, values='pop', names='country',
-       title='Population of Asian Continent',
-       color_discrete_sequence=px.colors.sequential.RdBu
-       )
-
-colors=['blue', 'green', 'black', 'purple', 'red', 'brown']
-fig = go.Figure(data=[go.Pie(labels=['Water', 'Grass', 'Normal', 'Psychic', 'Fire', 'Ground'], values=[110,90,80,80,70,60])])
-fig.update_traces(hoverinfo='label+percent', textfont_size=20, textinfo='label+percent',
-                  pull=[0.1, 0, 0.2, 0, 0, 0],
-                  marker=dict(colors=colors, line=dict(color='#FFFFFF', width=2))
-                  )
-
-# Histogram
-dice_1 = np.random.randint(1,7,5000)
-dice_2 = np.random.randint(1,7,5000)
-
-dice_sum = dice_1 + dice_2
-print(dice_sum)
-fig  = px.histogram(dice_sum, nbins=11,
-                    labels='5000 Dice Roll Histogram',
-                    marginal='violin',
-                    color_discrete_sequence=['green']
-                    )
-fig.update_layout(
-    xaxis_title_text='Dice Roll',
-    yaxis_title_text='Dice Sum',
-    bargap=0.2, showlegend=False)
-
-
-df_tips = px.data.tips()
-fig = px.histogram(df_tips, x='total_bill', color='sex')
-
-
-fig = px.box(df_tips, x='sex', y='tip', points='all')
-
-fig = px.box(df_tips, x='day', y='tip',color='sex')
-fig.show()
